cd2596cb86ef0070170b3d169c609a2d42139123
sight-315 <kd1228558@st.kobedenshi.ac.jp>
2018-01-31 02:56:07 +0000

    01/31 第3回課題提出

--- myapp/app/controllers/main_controller.rb
+++ myapp/app/controllers/main_controller.rb
@@ -1,4 +1,12 @@
 class MainController < ApplicationController
   def index
+    session[:gaman] ||= 0
+    @gaman = params[:result].to_i + session[:gaman]
+    session[:gaman] = @gaman
+  end
+  def index2
+    session[:gaman] = 0
+    redirect_to action: "index"
+ 
   end
 end
